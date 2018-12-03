0e8a9b4d700fae41f3a1f2177847511bf882756e
Connor Wright <connor@connorwright.uk>
2018-12-03 23:30:28 +0000

    Revert "Revert "better configuration system""
    This reverts commit 31f6ba118bc47478c49816440269f6a59a7e1542.

--- src/Modules/PingModule.cs
+++ src/Modules/PingModule.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace Dijon.Modules
 {
@@ -8,7 +11,14 @@ namespace Dijon.Modules
         [Command("ping"), Summary("Pings and pongs")]
         public async Task Ping()
         {
-            await ReplyAsync("Pong!");
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.Title = ":ping_pong: Pong!";
+            builder.Description = Environment.NewLine + ":heartbeat: **" + (Context.Client as DiscordSocketClient).Latency + " ms** heartbeat latency";
+            builder.Color = Color.Green;
+
+            await ReplyAsync("", false, builder);
         }
     }
 }
