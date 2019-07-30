895a1b5c0946228f6796b36b11578bab3709b415
Joshua Harms <joshua@nozzlegear.com>
2019-07-30 16:35:00 -0500

    Add placeholder functions for guild member updated/left/joined

--- src/Bot.fs
+++ src/Bot.fs
@@ -35,9 +35,23 @@ module Bot =
         iterate (collection.GetEnumerator()) []
 
     let private listGuildUsers (guild: Rest.RestGuild) = guild.GetUsersAsync() |> enumerate
+
     let private mapUsersToMembers (guild: Rest.RestGuild) =
         listGuildUsers guild 
         |> Async.SeqMap MemberUpdate.FromGuildUser 
+
+    let private handleUserLeft (user: SocketGuildUser) = 
+        Async.Empty
+
+    let private handleUserJoined (user: SocketGuildUser) = 
+        Async.Empty
+
+    let private handleUserUpdated (before: SocketGuildUser) (after: SocketGuildUser) = 
+        if before.Nickname = after.Nickname && before.Username = after.Username && before.Discriminator = after.Discriminator then
+            Async.Empty 
+        else
+            printfn "Guild member %s#%s with nickname %s is now %s" before.Username before.Discriminator before.Nickname after.Nickname
+            Async.Empty
 
     let Connect botToken = 
         let client = new DiscordSocketClient()
@@ -61,8 +75,6 @@ module Bot =
             |> Async.Parallel
             |> Async.Map Seq.concat
 
-        for m in guildMembers do printfn "Found member %s#%s in guild %A" m.Username m.Discriminator m.GuildId
-
         do! bot.database.BatchSetAsync guildMembers
         // TODO: the bot should look for members that may have left while it was offline and announce them to the configured channel
     }
@@ -70,20 +82,9 @@ module Bot =
     let WireEventListeners (bot: BotConfig) =
         let client = bot.client
 
-        // TODO: bot should listen for the following text
-        // 1. !dijon test - fakes a user leaving the server, printing the message out to the channel
-        // 2. !dijon verify - counts the number of members recorded in the databse for the current guild and returns the count to the channel. Should also print out what it thinks your current nickname is (to test changing nicknames).
-        // 3. !dijon status - prints out the bot's uptime and ping
-        // 4. !dijon member log here - tells Dijon to record this channel as the one it should output member changes to
-       
         client.add_MessageReceived += bot.messages.HandleMessage 
-        client.add_UserLeft += (fun u -> async {()})
-        client.add_UserJoined += (fun u -> async {()})
-        client.add_GuildMemberUpdated ++= (fun before after -> async {
-            if before.Nickname = after.Nickname && before.Username = after.Username && before.Discriminator = after.Discriminator then
-                ()
-            else
-                printfn "Guild member %s#%s with nickname %s is now %s" before.Username before.Discriminator before.Nickname after.Nickname
-        })
+        client.add_UserLeft += handleUserLeft
+        client.add_UserJoined += handleUserJoined
+        client.add_GuildMemberUpdated ++= handleUserUpdated
 
         Async.Empty
