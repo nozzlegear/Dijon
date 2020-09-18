66dad7b84e82553222cfb22b957353aaccfaa043
Joshua Harms <joshua@nozzlegear.com>
2020-09-18 16:43:34 -0500

    Split listing affixes into module

---
+++ src/Affixes.fs
@@ -0,0 +1,25 @@
+module Affixes
+    open Dijon
+    open System 
+    open System.Net.Http
+    
+    let list () : Async<Result<RaiderIo.ListAffixesResponse, string>> =
+        async {
+            use client = new HttpClient()
+            use req = new HttpRequestMessage()
+            req.Method <- HttpMethod.Get
+            req.RequestUri <- Uri "https://raider.io/api/v1/mythic-plus/affixes?region=us"
+            
+            let! result =
+                client.SendAsync req
+                |> Async.AwaitTask
+                
+            if not result.IsSuccessStatusCode then
+                let msg = sprintf "Failed to list affixes. Raider.io returned %i %s." (int result.StatusCode) result.ReasonPhrase
+                return Error msg
+            else
+                let! content =
+                    result.Content.ReadAsStringAsync()
+                    |> Async.AwaitTask
+                return Thoth.Json.Net.Decode.fromString RaiderIo.ListAffixesResponse.Decoder content
+        }
--- src/MessageHandler.fs
+++ src/MessageHandler.fs
@@ -324,30 +324,9 @@ type MessageHandler(database: IDijonDatabase, client: DiscordSocketClient) =
             |> mutliReact msg
             
     let handleGetAffixMessage (msg : IMessage) =
-        let listAffixes () : Async<Result<RaiderIo.ListAffixesResponse, string>> =
-            async {
-                use client = new HttpClient()
-                use req = new HttpRequestMessage()
-                req.Method <- HttpMethod.Get
-                req.RequestUri <- Uri "https://raider.io/api/v1/mythic-plus/affixes?region=us"
-                
-                let! result =
-                    client.SendAsync req
-                    |> Async.AwaitTask
-                    
-                if not result.IsSuccessStatusCode then
-                    let msg = sprintf "Failed to list affixes. Raider.io returned %i %s." (int result.StatusCode) result.ReasonPhrase
-                    return Error msg
-                else
-                    let! content =
-                        result.Content.ReadAsStringAsync()
-                        |> Async.AwaitTask
-                    return Thoth.Json.Net.Decode.fromString RaiderIo.ListAffixesResponse.Decoder content
-            }
-        
         async {
             let! editable = sendEditableMessage msg.Channel "Fetching affixes, please wait..."
-            let! affixList = listAffixes ()
+            let! affixList = Affixes.list()
             let editMessage (props : MessageProperties) =
                 let embed =
                     let builder = EmbedBuilder()
