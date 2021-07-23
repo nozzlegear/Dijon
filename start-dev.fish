c8301b6fbc61b1c0daec01937af1519c72754f07
Joshua Harms <joshua@nozzlegear.com>
2021-07-23 14:48:08 -0500

    Use sudo for docker command

--- start-dev.fish
+++ start-dev.fish
@@ -10,13 +10,13 @@ set connectionString "$DIJON_SQL_CONNECTION_STRING"
 set -x ASPNETCORE_ENVIRONMENT "development"
 
 # Check if the sql container exists
-if test (docker ps -a -f "name=$containerName" -q)
+if test (sudo docker ps -a -f "name=$containerName" -q)
     echo "Starting sql database container..."
-    docker start "$containerName"; or exit 1
+    sudo docker start "$containerName"; or exit 1
 else
     echo "Sql database container does not exist, creating it..."
     echo "Using sql password $sqlPassword"
-    docker run -d -it --name "$containerName" -e "SA_PASSWORD=$sqlPassword" -e "ACCEPT_EULA=Y" -p "$containerPort:1433" -v "$PWD/volumes/mssql:/var/opt/mssql" mcr.microsoft.com/mssql/server:2017-latest-ubuntu; or exit 1
+    sudo docker run -d -it --name "$containerName" -e "SA_PASSWORD=$sqlPassword" -e "ACCEPT_EULA=Y" -p "$containerPort:1433" -v "$PWD/volumes/mssql:/var/opt/mssql" mcr.microsoft.com/mssql/server:2017-latest-ubuntu; or exit 1
 end 
 
 dotnet watch --project src run -c Debug
