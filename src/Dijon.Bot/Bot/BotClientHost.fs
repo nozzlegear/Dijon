e18d13df95c163e8b5c2a87bbadef840d582c220
Joshua Harms <joshua@nozzlegear.com>
2023-10-25 23:18:22 -0500

    Add additional logging for bot initialization/disposal

--- src/Dijon.Bot/Bot/BotClientHost.fs
+++ src/Dijon.Bot/Bot/BotClientHost.fs
@@ -1,19 +1,23 @@
 namespace Dijon.Bot
 
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
 open System
 open System.Threading.Tasks
-open Microsoft.Extensions.Hosting
 
 type BotClientHost(
-    bot: IBotClient
+    bot: IBotClient,
+    logger: ILogger<BotClientHost>
 ) =
     interface IHostedService with
         member _.StartAsync cancellationToken =
+            logger.LogTrace("Initializing Bot")
             bot.InitAsync (cancellationToken)
 
         member _.StopAsync _ =
             match bot with
             | :? IAsyncDisposable as x ->
+                logger.LogTrace("Disposing Bot")
                 x.DisposeAsync().AsTask()
             | _ ->
                 Task.CompletedTask
