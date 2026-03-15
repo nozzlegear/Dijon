2a437e1305784d4b367363074384f56847e39c65
Joshua Harms <joshua@nozzlegear.com>
2026-03-15 10:51:34 -0500

    Switch dev container from MSSQL to PostgreSQL
    Replaces the SQL Server container setup with postgres:17-alpine on
    port 5432. Volume path updated to ~/.local/volumes/dijon/postgres.
    
    type: chore
    scope: dev

--- start-sql-container-dev.fish
+++ start-sql-container-dev.fish
@@ -1,32 +1,10 @@
 #! /usr/bin/env fish
 
-set volumeLocation "$HOME/.local/volumes/dijon/mssql"
-set dbImage "mcr.microsoft.com/mssql/server:2017-latest-ubuntu"
+set volumeLocation "$HOME/.local/volumes/dijon/postgres"
+set dbImage "postgres:17-alpine"
 set containerName "dijon_db"
-set containerPort "7011"
-set sqlPassword "a-BAD_passw0rd"
-set useSudoForDocker
-
-function isArm64
-    # Note that functions in fish return exit codes, not boolean true/false
-    if test (uname -m) = "arm64"
-        true
-    else
-        false
-    end
-end
-
-# Formats the database container's system user. If the host is arm64, this well set the user to root, which is required for the Azure SQL Edge db image.
-function formatDbSystemUser
-    if isArm64
-        echo "-u=root"
-    end
-end
-
-# If the user is on arm, change the db image to Azure SQL Edge, as the default mssql server does not currently support arm.
-if isArm64
-    set dbImage "mcr.microsoft.com/azure-sql-edge:1.0.5"
-end
+set containerPort "5432"
+set dbPassword "a-BAD_passw0rd"
 
 if test ! -d "$volumeLocation"
     mkdir -p "$volumeLocation"
@@ -35,7 +13,7 @@ end
 # Figure out whether to use podman, docker or sudo docker to start containers
 if command -q podman
     set USE_PODMAN 1
-else 
+else
     set USE_PODMAN 0
 
     # Check if the user can use Docker without sudo
@@ -44,12 +22,12 @@ else
     else if sudo docker ps &> /dev/null
         set USE_SUDO_FOR_DOCKER 1
     else
-        printErr "'podman', 'docker ps' and 'sudo docker ps' commands failed to return a successful exit code. Are Podman or Docker configured properly? Do 'podman ps', 'docker ps' or 'sudo docker ps' work?"
+        echo "'podman', 'docker ps' and 'sudo docker ps' commands failed to return a successful exit code. Are Podman or Docker configured properly? Do 'podman ps', 'docker ps' or 'sudo docker ps' work?"
         exit 1
     end
 end
 
-function pod 
+function pod
     if test $USE_PODMAN -eq 1
         podman $argv
     else if test $USE_SUDO_FOR_DOCKER -eq 1
@@ -66,15 +44,14 @@ if test (pod ps -a -f "name=$containerName" -q)
     or exit 1
 else
     echo "Container $containerName does not exist, creating it..."
-    echo "Using sql password $sqlPassword"
+    echo "Using password $dbPassword"
     pod run \
         -dit \
         --name "$containerName" \
-        -e "SA_PASSWORD=$sqlPassword" \
-        -e "ACCEPT_EULA=Y" \
-        -p "$containerPort:1433" \
-        -v "$volumeLocation:/var/opt/mssql" \
-        (formatDbSystemUser) \
+        -e "POSTGRES_PASSWORD=$dbPassword" \
+        -e "POSTGRES_DB=dijon" \
+        -p "$containerPort:5432" \
+        -v "$volumeLocation:/var/lib/postgresql/data" \
         "$dbImage"
     or exit 1
-end 
+end
